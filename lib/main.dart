bc273bdbd5942cdcf7b192894780fd9229b2dcff
bigbenbigben <positive.feedback.hh@gmail.com>
2024-11-11 14:24:41 +0900

    タイマー作成、SEと停止は作成中

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,258 @@
-import 'package:flutter/material.dart';
+import 'dart:ffi';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TimerPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class TimerPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _TimerPageState createState() => _TimerPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TimerPageState extends State<TimerPage> {
+  Duration _duration = Duration(hours: 0, minutes: 0, seconds: 0);
+  Timer? _timer;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void _startTimer() {
+    if (_timer != null) {
+      _timer!.cancel();
+    }
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        if (_duration.inSeconds > 0) {
+          _duration -= Duration(seconds: 1);
+        } else {
+          _timer!.cancel();
+        }
+      });
     });
+  }
+
+  void _showTimePicker() {
+    Duration tempDuration = _duration; // 選択された時間を一時保存
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Column(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // キャンセルボタン
+                  ElevatedButton(
+                    onPressed: () => Navigator.pop(context),
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                    ),
+                    child: Text('キャンセル'),
+                  ),
+                  // タイトル
+                  Center(
+                    child: Text(
+                      'タイマー設定',
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  // OKボタン
+                  ElevatedButton(
+                    onPressed: () {
+                      if (tempDuration.inHours < 0 ||
+                          tempDuration.inMinutes >= 60 ||
+                          tempDuration.inSeconds >= 60) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('不正な値が設定されました'),
+                          ),
+                        );
+                      } else {
+                        setState(() {
+                          _duration = tempDuration;
+                        });
+                        Navigator.pop(context);
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                    ),
+                    child: Text('OK'),
+                  ),
+                ],
+              ),
+            ),
+            Divider(),
+            // ダイアル
+            Expanded(
+              child: Container(
+                width: MediaQuery.of(context).size.width, // 画面いっぱいの幅
+                height: MediaQuery.of(context).size.height * 0.5, // 画面高さの50%を指定
+                child: CupertinoTimerPicker(
+                  initialTimerDuration: _duration,
+                  mode: CupertinoTimerPickerMode.hms,
+                  onTimerDurationChanged: (Duration newDuration) {
+                    tempDuration = newDuration;
+                  },
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _stopTimer() {
+    //TODO
+    
+    // if (_timer != null) {
+    //   _timer!.cancel();
+    // }
+    // _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+    //   setState(() {
+    //     if (_duration.inSeconds > 0) {
+    //       _duration -= Duration(seconds: 1);
+    //     } else {
+    //       _timer!.cancel();
+    //     }
+    //   });
+    // });
+  }
+
+  String _formatDuration(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, "0");
+    String hours = twoDigits(duration.inHours);
+    String minutes = twoDigits(duration.inMinutes.remainder(60));
+    String seconds = twoDigits(duration.inSeconds.remainder(60));
+    return "$hours:$minutes:$seconds";
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // 画面に合わせたフォントサイズ
+    // タイトル
+    double fontSize_title = MediaQuery.of(context).size.width * 0.15;
+    // タイマー
+    double fontSize_timer = MediaQuery.of(context).size.width * 0.25;
+    // ボタン
+    double fontSize_timer_button = MediaQuery.of(context).size.width * 0.10;
+    // 画面に合わせた配置
+    // ボタン幅と高さ
+    double widthsize_button = MediaQuery.of(context).size.width * 0.75;
+    double heightsize_button = MediaQuery.of(context).size.height * 0.10;
+    // ボタン配置
+    // double widthsize_button_position = MediaQuery.of(context).size.width * 0.75;
+    double heightsize_button_position = MediaQuery.of(context).size.height * 0.025;
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Column(
+          children: [
+            Center(child: SizedBox(child: Text(
+              'タイマー',
+              style: TextStyle(fontSize: fontSize_title),  // 画面幅に基づいたフォントサイズ
+              ))),
+          ],
+        ),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              _formatDuration(_duration),
+              style: TextStyle(fontSize: fontSize_timer),
+            ),
+            SizedBox(
+              // width: widthsize_button,
+              // height: heightsize_button,
+              ),
+            ElevatedButton(
+              onPressed: _showTimePicker,
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10), // 角丸の半径
+                ),
+                padding: EdgeInsets.all(3.0), // ボタン全体の余白を設定
+              ),
+              child: SizedBox(
+                width: widthsize_button,
+                // height: heightsize_button,
+                child: Center(
+                  child: Text(
+                    '時間を設定',
+                    style: TextStyle(fontSize: fontSize_timer_button), // 画面幅に基づいたフォントサイズ
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(
+              width: widthsize_button,
+              height: heightsize_button_position,
+              ),
+            ElevatedButton(
+              onPressed: _showTimePicker,
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10), // 角丸の半径
+                ),
+                padding: EdgeInsets.all(3.0), // ボタン全体の余白を設定
+              ),
+              child: SizedBox(
+                width: widthsize_button,
+                // height: heightsize_button,
+                child: Center(
+                  child: Text(
+                    'タイマー開始',
+                    style: TextStyle(fontSize: fontSize_timer_button), // 画面幅に基づいたフォントサイズ
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(
+              width: widthsize_button,
+              height: heightsize_button_position * 4,
+              ),
+            ElevatedButton(
+              onPressed: _stopTimer,
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10), // 角丸の半径
+                ),
+                padding: EdgeInsets.all(3.0), // ボタン全体の余白を設定
+              ),
+              child: SizedBox(
+                width: widthsize_button,
+                // height: heightsize_button,
+                child: Center(
+                  child: Text(
+                    '停　止',
+                    style: TextStyle(fontSize: fontSize_timer_button), // 画面幅に基づいたフォントサイズ
+                  ),
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
